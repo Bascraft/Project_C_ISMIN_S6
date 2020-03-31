e16085680f58f835ac1d833eb47925eccad987d0
Bascraft <45175867+Bascraft@users.noreply.github.com>
2020-04-01 00:02:23 +0200

    Add files via upload

--- projet Jeux/src/main.c
+++ projet Jeux/src/main.c
@@ -3,33 +3,33 @@
 
 
 //#include "blockchain.h"
-#include "/home/herrrouin/Bureau/ProjetC_Bastien/lib/SDL_env.h"
+#include "../lib/SDL_env.h"
 
 
 
 int main(int argc, char* args[])
 {
-    init_SDL();
+	init_SDL();
     SDL_Surface *screen;
     Sprite *perso;
     perso = InitialiserSprite(320,240,320,240,"image_block/perso.bmp");
+	Map W;
     SDL_Event event;
-    Map map;
-    int quit = 0;
-    ChargerMap_level("level/level_prop.txt","level/level.txt",&map);
+    int quit_menu = 0;
+    int quit_game = 1;
+    MainMenu menu;
     screen = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE|SDL_DOUBLEBUF); // Ouverture de la fenêtre
-    SDL_WM_SetCaption("Blockcraft XD", NULL);//faut choisir un nom :)
-    while (!quit)
-    {   
-        Bouge_sprite(event,perso,&quit,map);
-        Afficher(screen,map,perso);
-        affichersprite(screen,perso);
-        SDL_Flip(screen);
-    }
-    
-    
-    
-    //pause(); // Mise en pause du programme
+	    
+    SDL_WM_SetCaption("Fantom Building", NULL);//faut choisir un nom :)
+	while (!quit_menu){
+        main_Menu(&menu,event,&quit_menu,&quit_game,screen,640,480);
+        SDL_Flip(screen);}
+	if (quit_game == 2){
+        ChargerMap_level("level/level_prop.txt","level/level.txt",&W);
+    	world("level/level_prop.txt","level/level.txt",screen,event,perso,W);}
+		
+	
+
     SDL_Quit(); // Arrêt de la SDL
     return EXIT_SUCCESS; // Fermeture du programme
 }
